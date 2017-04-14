3131c7c427fae46094c229f7b6e453bae5118718
weathertopper <nathanweatherly94@gmail.com>
2017-04-14 00:58:06 -0400

    adding a good start to event list styling in relative show, but got a ways to go. change this to a three-col table with a line and dots through the middle column

--- rails-project/PhotoFamilyTree/config/initializers/assets.rb
+++ rails-project/PhotoFamilyTree/config/initializers/assets.rb
@@ -12,4 +12,5 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( relative_tile.scss.css )
 Rails.application.config.assets.precompile += %w( relatives.scss.css )
-Rails.application.config.assets.precompile += %w( relative-show.scss.css )
+Rails.application.config.assets.precompile += %w( relative_show.scss.css )
+Rails.application.config.assets.precompile += %w( relative_event_timeline.scss.css )
